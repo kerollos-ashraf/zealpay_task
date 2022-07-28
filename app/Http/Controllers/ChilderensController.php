50a61d8b802c23bc6fc83d838377a908000d03cf
kerollos-ashraf <kerollos117@gmail.com>
2022-07-28 04:10:38 +0200

    update models -contolers - migrations - register and login

---
+++ app/Http/Controllers/ChilderensController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Childerens;
+use Illuminate\Http\Request;
+
+class ChilderensController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Childerens  $childerens
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Childerens $childerens)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Childerens  $childerens
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Childerens $childerens)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Childerens  $childerens
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Childerens $childerens)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Childerens  $childerens
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Childerens $childerens)
+    {
+        //
+    }
+}
